98a3d31b7bf2276fef3328cec19cf981cb94e2a4
Agrim Rawat <rawat.agrim432@gmail.com>
2022-02-22 22:49:31 +0530

    Modified decode

--- SEQ/decode.v
+++ SEQ/decode.v
@@ -1,106 +1,98 @@
-`timescale 1ns/1ps
+	module decode(clk, icode, rA, rB, valA, valB);
 
-module decode(
-    clk,
-    icode,
-    rA,
-    rB,
-    valA,
-    valB,
-);
+	input             clk;
+	input      [ 3:0] icode;
+	input      [ 3:0] rA;
+	input      [ 3:0] rB;
+	output reg [63:0] valA;
+	output reg [63:0] valB;
 
-input clk;
-input [3:0] icode;
-input [3:0] rA;
-input [3:0] rB;
-output reg [63:0] valA;
-output reg [63:0] valB;
-reg [63:0] register_memory[0:14];
+	reg [63:0] register_file[0:14];
+	/* register_memory[0]  -> %rax
+	   register_memory[1]  -> %rcx
+	   register_memory[2]  -> %rdx
+	   register_memory[3]  -> %rbx
+	   register_memory[4]  -> %rsp
+	   register_memory[5]  -> %rbp
+	   register_memory[6]  -> %rsi
+	   register_memory[7]  -> %rdi
+	   register_memory[8]  -> %r8
+	   register_memory[9]  -> %r9
+	   register_memory[10] -> %r10
+	   register_memory[11] -> %r11
+	   register_memory[12] -> %r12
+	   register_memory[13] -> %r13
+	   register_memory[14] -> %r14 */
 
-/*
+	// For testing purposes, hardcoding the values stored in the registers
+	initial begin
+		register_file[0] = 64'd0;
+		register_file[1] = 64'd1;
+		register_file[2] = 64'd2;
+		register_file[3] = 64'd3;
+		register_file[4] = 64'd4;
+		register_file[5] = 64'd5;
+		register_file[6] = 64'd7;
+		register_file[7] = 64'd7;
+		register_file[8] = 64'd8;
+		register_file[9] = 64'd9;
+		register_file[10] = 64'd10;
+		register_file[11] = 64'd11;
+		register_file[12] = 64'd12;
+		register_file[13] = 64'd13;
+		register_file[14] = 64'd14;
+	end
 
-    register_memory[0] is %rax
-    register_memory[1] is %rcx
-    register_memory[2] is %rdx
-    register_memory[3] is %rbx
-    register_memory[4] is %rsp
-    register_memory[5] is %rbp
-    register_memory[6] is %rsi
-    register_memory[7] is %rdi
-    register_memory[8] is %r8
-    register_memory[9] is %r9
-    register_memory[10] is %r10
-    register_memory[11] is %r11
-    register_memory[12] is %r12
-    register_memory[13] is %r13
-    register_memory[14] is %r14
+	always @(posedge clk) begin
+		// cmovxx rA, rB
+		if (icode == 4'b0010) begin
+			valA = register_file[rA];
+		end
 
-*/
+		// irmovq V, rB
+		else if(icode == 4'b0011) begin
+			valB = register_file[rB];
+		end
 
-// For testing purposes, hardcoding the values stored in the registers
-initial begin
-    register_memory[0] = 64'd0;
-    register_memory[1] = 64'd1;
-    register_memory[2] = 64'd2;
-    register_memory[3] = 64'd3;
-    register_memory[4] = 64'd4;
-    register_memory[5] = 64'd5;
-    register_memory[6] = 64'd7;
-    register_memory[7] = 64'd7;
-    register_memory[8] = 64'd8;
-    register_memory[9] = 64'd9;
-    register_memory[10] = 64'd10;
-    register_memory[11] = 64'd11;
-    register_memory[12] = 64'd12;
-    register_memory[13] = 64'd13;
-    register_memory[14] = 64'd14;
-end
+		// rmmovq rA, D(rB)
+		else if(icode == 4'b0100) begin
+			valA = register_file[rA];
+			valB = register_file[rB];
+		end
 
-always@(posedge clk)
-begin
+		// mrmovq D(rB), rA
+		else if(icode == 4'b0101) begin
+			valB = register_file[rB];
+		end
 
-    if (icode == 4'b0010)   // cmovXX
-    begin
-        valA = register_memory[rA];
-    end
-    else if(icode == 4'b0011)   // irmovq
-    begin
-        valB = register_memory[rB];
-    end
-    else if(icode == 4'b0100)   // rmmovq
-    begin
-        valA = register_memory[rA];
-        valB = register_memory[rB];
-    end
-    else if(icode == 4'b0101)   // mrmovq
-    begin
-        valB = register_memory[rB];
-    end 
-    else if(icode == 4'b0110)   // OPq
-    begin
-        valA = register_memory[rA];
-        valB = register_memory[rB];
-    end
-    else if(icode == 4'b1000)   // call
-    begin
-        valB = register_memory[4];
-    end
-    else if(icode == 4'b1001)   // ret
-    begin
-        valA = register_memory[4];
-        valB = register_memory[4];
-    end 
-    else if(icode == 4'b1010)   // pushq
-    begin
-        valA = register_memory[rA];
-        valB = register_memory[4];
-    end
-    else if(icode == 4'b1011)   // popq
-    begin
-        valA = register_memory[4];
-        valB = register_memory[4];
-    end    
+		// OPq rA, rB
+		else if(icode == 4'b0110) begin
+			valA = register_file[rA];
+			valB = register_file[rB];
+		end
 
-end
+		// call Dest
+		else if(icode == 4'b1000) begin
+			valB = register_file[4];
+		end
+
+		// ret
+		else if(icode == 4'b1001) begin
+			valA = register_file[4];
+			valB = register_file[4];
+		end
+
+		// pushq rA, rB
+		else if(icode == 4'b1010) begin
+			valA = register_file[rA];
+			valB = register_file[4];
+		end
+
+		// popq rA, rB
+		else if(icode == 4'b1011) begin
+			valA = register_file[4];
+			valB = register_file[4];
+		end
+	end
 
 endmodule
--- SEQ/decode_tb.v
+++ SEQ/decode_tb.v
@@ -1,55 +1,51 @@
-module decode_tb();
+`timescale 1ns / 1ps
 
-reg clk;
-reg [3:0] icode;
-reg [3:0] rA;
-reg [3:0] rB; 
-output [63:0] valA;
-output [63:0] valB;
+module decode_tb;
 
-decode decode(
-    .clk(clk),
-    .icode(icode),
-    .rA(rA),
-    .rB(rB),
-    .valA(valA),
-    .valB(valB)
-  );
+	reg clk;
+	reg [3:0] icode;
+	reg [3:0] rA;
+	reg [3:0] rB;
+	output [63:0] valA;
+	output [63:0] valB;
 
-initial begin
+	decode uut (clk, icode, rA, rB, valA, valB);
 
-    clk=1'b0;
+	initial begin
+		$dumpfile("decode_tb.vcd");
+		$dumpvars(0, decode_tb);
 
-    icode = 4'b0010; rA = 4'b0010; rB = 4'b0010;
-    #10 clk = ~clk; 
-    #10 clk = ~clk;
-    icode = 4'b0011; rA = 4'b0000; rB = 4'b1000;
-    #10 clk = ~clk; 
-    #10 clk = ~clk;
-    icode = 4'b0100; rA = 4'b1100; rB = 4'b0101;
-    #10 clk = ~clk; 
-    #10 clk = ~clk;
-    icode = 4'b0101; rA = 4'b1110; rB = 4'b1001;
-    #10 clk = ~clk; 
-    #10 clk = ~clk;
-    icode = 4'b0110; rA = 4'b0011; rB = 4'b0001;
-    #10 clk = ~clk; 
-    #10 clk = ~clk;
-    icode = 4'b1000; rA = 4'b0000; rB = 4'b1110;
-    #10 clk = ~clk; 
-    #10 clk = ~clk;
-    icode = 4'b1001; rA = 4'b0100; rB = 4'b1001;
-    #10 clk = ~clk; 
-    #10 clk = ~clk;
-    icode = 4'b1010; rA = 4'b0101; rB = 4'b1101;
-    #10 clk = ~clk; 
-    #10 clk = ~clk;
-    icode = 4'b1011; rA = 4'b0000; rB = 4'b0000;
-    #10 clk = ~clk; 
-    #10 clk = ~clk;
+		clk=1'b0;
+		icode = 4'b0010; rA = 4'b0010; rB = 4'b0010;
+		#10 clk = ~clk;
+		#10 clk = ~clk;
+		icode = 4'b0011; rA = 4'b0000; rB = 4'b1000;
+		#10 clk = ~clk;
+		#10 clk = ~clk;
+		icode = 4'b0100; rA = 4'b1100; rB = 4'b0101;
+		#10 clk = ~clk;
+		#10 clk = ~clk;
+		icode = 4'b0101; rA = 4'b1110; rB = 4'b1001;
+		#10 clk = ~clk;
+		#10 clk = ~clk;
+		icode = 4'b0110; rA = 4'b0011; rB = 4'b0001;
+		#10 clk = ~clk;
+		#10 clk = ~clk;
+		icode = 4'b1000; rA = 4'b0000; rB = 4'b1110;
+		#10 clk = ~clk;
+		#10 clk = ~clk;
+		icode = 4'b1001; rA = 4'b0100; rB = 4'b1001;
+		#10 clk = ~clk;
+		#10 clk = ~clk;
+		icode = 4'b1010; rA = 4'b0101; rB = 4'b1101;
+		#10 clk = ~clk;
+		#10 clk = ~clk;
+		icode = 4'b1011; rA = 4'b0000; rB = 4'b0000;
+		#10 clk = ~clk;
+		#10 clk = ~clk;
+	end
 
-end 
-  
-initial 
-	$monitor("clk = %d icode = %b rA = %b rB = %b valA = %g valB = %g\n",clk,icode,rA,rB,valA,valB);
+	initial
+		$monitor($time, " clk = %d icode = %b rA = %b rB = %b valA = %g valB = %g\n", clk,icode, rA, rB, valA, valB);
+
 endmodule
